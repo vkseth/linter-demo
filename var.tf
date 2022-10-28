8229d35982ed3d3fa4ed0c2970551720365ac3e4
vicky <vicky@gmail.com>
2022-10-28 09:51:49 +0000

    code upate

--- var.tf
+++ var.tf
@@ -1,12 +1,15 @@
 variable "access_key" {
   default = "AKIAWZ2ZZLLG26YVC6NO"
+  type        = string
 
 }
 variable "secret_key" {
   default = "ezy6GwqPGe3zLBjh56YdLpnxa0iLZyDUnN5cXG1q"
+  type        = string
 
 }
 variable "region" {
   default = "us-east-1"
+  type        = string
 
 }
