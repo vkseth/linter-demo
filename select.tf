29b38f64f4a644ae2ccd785b4c6a9e0fb460bcd5
vicky <vicky@gmail.com>
2022-10-28 09:44:06 +0000

    proper code

---
+++ select.tf
@@ -0,0 +1,12 @@
+# Selection for default workloads
+resource "aws_backup_selection" "selection_default" {
+  iam_role_arn = aws_iam_role.role_backup.arn
+  name         = "backup_selection_default"
+  plan_id      = aws_backup_plan.backup_plan_default.id
+  selection_tag {
+    type  = "STRINGEQUALS"
+    key   = "backup_plan"
+    value = "default"
+  }
+}
+
---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "access_key" {
+  default = "AKIAWZ2ZZLLG26YVC6NO"
+
+}
+variable "secret_key" {
+  default = "ezy6GwqPGe3zLBjh56YdLpnxa0iLZyDUnN5cXG1q"
+
+}
+variable "region" {
+  default = "us-east-1"
+
+}
