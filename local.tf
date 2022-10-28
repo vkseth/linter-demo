66d50e041039f78f87787579918efdc62fc52fa8
EC2 Default User <ec2-user@ip-172-31-46-126.ap-south-1.compute.internal>
2022-10-28 09:08:57 +0000

    initial commit

---
+++ local.tf
@@ -0,0 +1,10 @@
+locals {
+
+  region = "us-east-1"
+  # Default Backup plan
+  #plan_schedule_default      = "cron(0 12 * * ? *)" # every day at 12:00
+  plan_schedule_default      = "cron(0 12 * * ? *)" # once every 10 minutes for testing
+  cold_storage_after_default = 30
+  delete_after_default       = 120
+
+}
